ed561ba81d6fa73a58d6fbdae3b781a081eb54ef
Kiryuu Sakuya <misaka00251@misakanet.cn>
2020-04-17 19:56:45 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# OpenSearch addon for 绅士仓库
+
+Search cangku in firefox.
+
+## How to build
+
+Run `makexpi.sh` on any Linux & BSD. You need to install 7zip first.
+
+## License
+
+WTFPL
---
+++ makexpi.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Note: Version needs to be changed here to set it in pacakage.json, and in install.rdf for a release build. The .ignore file contains which files will not be added to xpi.
+VERSION="1.0"
+if [ ! -d "$DIRECTORY" ]; then
+	mkdir dist
+fi
+cd dist
+7z a -tzip -mx9 -mm=Deflate -mfb=258 -mmt=8 -mpass=15 -mtc=on "cangku_opensearch-$VERSION-fx.xpi" ../* -x@../.ignore
+cd ..
+echo "Generated "cangku_opensearch-$VERSION-fx.xpi" placed in dist/"
+sha512sum dist/*
+echo "Done!"
