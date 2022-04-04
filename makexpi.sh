409b04ebf6079496a31b77e7d2ad199af1e8bb74
Kiryuu Sakuya <251@rbq.pw>
2022-04-05 00:30:39 +0800

    Update version

--- makexpi.sh
+++ makexpi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Note: Version needs to be changed here to set it in pacakage.json, and in install.rdf for a release build. The .ignore file contains which files will not be added to xpi.
-VERSION="3.0"
+VERSION="4.0"
 if [ ! -d "$DIRECTORY" ]; then
 	mkdir dist
 fi
